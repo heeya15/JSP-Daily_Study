853c1915c37e7a2d50ee5c2fce7a23c983014bb4
KimKwangHui <hgi7201@naver.com>
2021-03-16 10:25:58 +0900

    서블릿을 통한 get, post 요청 처리방법 실습

---
+++ Practice file/JSPPrj/src/com/newlecture/web/Calculator.java
@@ -0,0 +1,21 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calculator") //사용자가 여기 URL로 요청
+public class Calculator extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGET 메소드가 호출 되었습니다.");
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doPOST 메소드가 호출 되었습니다.");
+	}
+}
